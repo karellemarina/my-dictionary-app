1dd9b4b65e2a1c333d7acdaa66ccf6e0d82d4c5f
karelle <k.bolon.m@gmail.com>
2024-09-29 09:45:36 +0200

    added body color same as logo background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from "./logo.png";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
 }
-
-export default App;
